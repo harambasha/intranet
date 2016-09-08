46c0523b35fc1d9aa2146b48732f898723e83ccb
harambasha <ismirharambasic@gmail.com>
2016-09-08 15:56:16 +0200

    Change tak publish

--- server/publish/tasks.js
+++ server/publish/tasks.js
@@ -1,12 +1,12 @@
 Meteor.publish("task_list", function() {
-	return Tasks.publishJoinedCursors(Tasks.find({ownerId:this.userId}, {sort:[["taskNumber","desc"]]}));
+	return Tasks.publishJoinedCursors(Tasks.find({}, {sort:[["taskNumber","desc"]]}));
 });
 
 Meteor.publish("tasks_empty", function() {
-	return Tasks.publishJoinedCursors(Tasks.find({_id:null,ownerId:this.userId}, {}));
+	return Tasks.publishJoinedCursors(Tasks.find({_id:null}, {}));
 });
 
 Meteor.publish("task_details", function(taskId) {
-	return Tasks.publishJoinedCursors(Tasks.find({_id:taskId,ownerId:this.userId}, {}));
+	return Tasks.publishJoinedCursors(Tasks.find({_id:taskId}, {}));
 });
 
